87854fe358586d5b1b09aad1b3b27f057b496fbe
rajmouni <lalithamethukula@gmail.com>
2018-03-29 11:52:12 +0530

    initial commit

--- main.js
+++ main.js
@@ -19,6 +19,8 @@ loadjson("data.json",function(text)
 let data=JSON.parse(text);
 console.log(data);
 basics(data.left);
+education(data.Education);
+skill(data.skills);
 })
 // for main div calling
 // var main=document.queryselector('.main');
@@ -29,7 +31,7 @@ var left=document.createElement('div');
 // add class name to element
 left.classList.add("left");
 // add id to element
-left.setAttribute("id","left");
+// left.setAttribute("id","left");
 // appending to main div
 left.textContent="Profile Details:";
 left.appendChild(document.createElement("HR"));
@@ -49,4 +51,51 @@ left.appendChild(email);
 var mobile=document.createElement("h3");
 mobile.textContent=leftside.mobile;
 left.appendChild(mobile);
+console.log(left);
+}
+var right=document.createElement("div");
+right.classList.add("right");
+left.setAttribute("id","right");
+main.appendChild(right);
+var edu=document.createElement("div");
+edu.classList.add("edu1")
+edu.textContent="Education Details:";
+edu.appendChild(document.createElement("HR"));
+right.appendChild(edu);
+function education(Educa)
+{
+for(i in Educa)
+{
+  var e1=document.createElement("div");
+  e1.classList.add("edu2");
+  e1.textContent=Educa[i].course;
+  var ul=document.createElement("ul");
+  for(j in Educa[i].college)
+  {
+    var li=document.createElement("li");
+li.textContent=Educa[i].college[j];
+ul.appendChild(li);
+e1.appendChild(ul);
+edu.appendChild(e1);
+console.log(right);
+}
+}
+}
+function skill(skills)
+{
+  var skill_tittle=document.createElement("div");
+  skill_tittle.classList.add("teach_skills");
+  skill_tittle.textContent="Techniqual Skills:";
+  skill_tittle.appendChild(document.createElement("HR"));
+  right.appendChild(skill_tittle);
+
+// table operation
+var table=document.createElement("table");
+var row="";
+for(var i=0; i<skills.length;i++)
+{
+  row=row+"<tr><td>"+skills[i].name+"</td><td>"+skills[i].value+"</td></tr>"
+}
+table.innerHTML=row;
+skill_tittle.appendChild(table);
 }
